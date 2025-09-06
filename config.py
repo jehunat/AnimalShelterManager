9e7ee19a7e7a8c167167fd8aeefb36e6f64fe1b1
jehunat <jonatan.kisvarga@gmail.com>
2025-09-06 20:47:12 +0200

    database.ini replaced by custom .env and config.py created

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def config():
+    db ={
+        "host": os.getenv("DB_HOST"),
+        "database": os.getenv("DB_NAME"),
+        "user": os.getenv("DB_USER"),
+        "password": os.getenv("DB_PASSWORD")
+    }
+    return db
