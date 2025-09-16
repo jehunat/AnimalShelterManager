58ee024afb2d2647a942fbc735f1eb8f52379e25
dgila <79481648+dgila@users.noreply.github.com>
2025-09-16 22:53:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# ez szep. tegyel ra valamit, hogy itt elhasaljon, ha valami hianyzik. Aztan ha valami nem jo, akkor elfailel connectionnal
 def db_config():
     db ={
         "host": os.getenv("DB_HOST"),
